23570ca5f9788837ba73c6d5da5cf7194562244a
Georgios Verigakis <verigak@gmail.com>
2022-03-08 16:02:57 +0200

    Refactor to commit attachments in the repo
    - Switch to go-git instead of shelling out
    - Add some missing API calls

---
+++ backend.go
@@ -0,0 +1,435 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"path/filepath"
+	"sort"
+	"strings"
+	"time"
+
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/plumbing/filemode"
+	"github.com/go-git/go-git/v5/plumbing/object"
+)
+
+type Backend struct {
+	Trench string
+	r      *git.Repository
+}
+
+func NewBackend(root, trench string) (*Backend, error) {
+	gitDir := filepath.Join(root, trench)
+	r, err := git.PlainOpen(gitDir)
+	if errors.Is(err, git.ErrRepositoryNotExists) {
+		r, err = git.PlainInit(gitDir, true)
+	}
+	if err != nil {
+		return nil, fmt.Errorf("Failed to open repository for '%s': %w", trench, err)
+	}
+	b := &Backend{
+		Trench: trench,
+		r:      r,
+	}
+	return b, nil
+}
+
+func (b *Backend) ExistsAttachment(name, checksum string) bool {
+	refName := fmt.Sprintf("refs/attachments/%s/%s", name, checksum)
+	_, err := b.r.Reference(plumbing.ReferenceName(refName), false)
+	return err == nil
+}
+
+type TrenchVersion struct {
+	Version string    `json:"version"`
+	Date    time.Time `json:"date"`
+}
+
+func (b *Backend) ListVersions() ([]TrenchVersion, error) {
+	var versions []TrenchVersion
+	it, err := b.r.Log(&git.LogOptions{})
+	if err != nil {
+		return nil, fmt.Errorf("Error getting version list: %w", err)
+	}
+	err = it.ForEach(func(c *object.Commit) error {
+		version := TrenchVersion{
+			Version: c.Hash.String(),
+			Date:    c.Author.When,
+		}
+		versions = append(versions, version)
+		return nil
+	})
+	if err != nil {
+		return nil, fmt.Errorf("Error getting version list: %w", err)
+	}
+	return versions, nil
+}
+
+func (b *Backend) ReadAttachment(name, checksum string) ([]byte, error) {
+	refName := fmt.Sprintf("refs/attachments/%s/%s", name, checksum)
+	ref, err := b.r.Reference(plumbing.ReferenceName(refName), false)
+	if err != nil {
+		return nil, fmt.Errorf("Attachment '%s' not found: %w", name, err)
+	}
+	return b.readBlob(ref.Hash())
+}
+
+func (b *Backend) ReadSurveys() ([]Survey, error) {
+	head := b.Head()
+	if head == "" {
+		return nil, nil
+	} else {
+		return b.ReadSurveysAtVersion(head)
+	}
+}
+
+func (b *Backend) ReadSurveysAtVersion(version string) ([]Survey, error) {
+	h := plumbing.NewHash(version)
+	commit, err := b.r.CommitObject(h)
+	if err != nil {
+		return nil, fmt.Errorf("Invalid version %s", version)
+	}
+	rootTree, err := b.r.TreeObject(commit.TreeHash)
+	if err != nil {
+		return nil, err
+	}
+	surveysTree, err := rootTree.Tree("surveys")
+	if err != nil {
+		return nil, err
+	}
+	var surveys []Survey
+	for _, e := range surveysTree.Entries {
+		if strings.HasPrefix(e.Name, ".") || !e.Mode.IsFile() {
+			log.Printf("Warning: skipping %s", e.Name)
+			continue
+		}
+		data, err := b.readBlob(e.Hash)
+		if err != nil {
+			return nil, fmt.Errorf("Error reading survey %s: %w", e.Name, err)
+		}
+		var survey Survey
+		err = json.Unmarshal(data, &survey)
+		if err != nil {
+			return nil, fmt.Errorf("Error reading survey %s: %w", e.Name, err)
+		}
+		surveys = append(surveys, survey)
+	}
+	return surveys, nil
+}
+
+func (b *Backend) ReadSurveyAtVersion(id, version string) (Survey, error) {
+	h := plumbing.NewHash(version)
+	commit, err := b.r.CommitObject(h)
+	if err != nil {
+		return nil, fmt.Errorf("Invalid version %s", version)
+	}
+	rootTree, err := b.r.TreeObject(commit.TreeHash)
+	if err != nil {
+		return nil, err
+	}
+	surveysTree, err := rootTree.Tree("surveys")
+	if err != nil {
+		return nil, err
+	}
+	name := fmt.Sprintf("%s.survey", id)
+	for _, e := range surveysTree.Entries {
+		if e.Name == name {
+			data, err := b.readBlob(e.Hash)
+			if err != nil {
+				return nil, fmt.Errorf("Error reading survey %s: %w", id, err)
+			}
+			var survey Survey
+			err = json.Unmarshal(data, &survey)
+			return survey, err
+		}
+	}
+	return nil, fmt.Errorf("Survey %s not found", id)
+}
+
+func (b *Backend) ReadAllSurveyVersions(id string) ([]SurveyVersion, error) {
+	var versions []SurveyVersion
+	filename := fmt.Sprintf("surveys/%s.survey", id)
+	lo := &git.LogOptions{
+		FileName: &filename,
+	}
+	it, err := b.r.Log(lo)
+	if err != nil {
+		return nil, fmt.Errorf("Error getting version list: %w", err)
+	}
+	err = it.ForEach(func(c *object.Commit) error {
+		v := c.Hash.String()
+		s, err := b.ReadSurveyAtVersion(id, v)
+		if err != nil {
+			return err
+		}
+		version := SurveyVersion{
+			Version: v,
+			Date:    c.Author.When,
+			Survey:  s,
+		}
+		versions = append(versions, version)
+		return nil
+	})
+	return versions, err
+}
+
+func (b *Backend) WriteAttachment(name, checksum string, data []byte) error {
+	h, err := b.addBlob(data)
+	if err != nil {
+		return err
+	}
+
+	refName := fmt.Sprintf("refs/attachments/%s/%s", name, checksum)
+	ref := plumbing.NewReferenceFromStrings(refName, h.String())
+	err = b.r.Storer.SetReference(ref)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (b *Backend) WriteTrench(uid, username, message string, surveys []Survey) (string, error) {
+	var entries []object.TreeEntry
+	var attachmentEntries []object.TreeEntry
+
+	for _, survey := range surveys {
+		id := survey.ID()
+		data, err := json.MarshalIndent(survey, "", "  ")
+		if err != nil {
+			return "", fmt.Errorf("Failed to write survey %s: %w", id, err)
+		}
+		name := id + ".survey"
+		h, err := b.addBlob(data)
+		if err != nil {
+			return "", fmt.Errorf("Failed to write survey %s data: %w", id, err)
+		}
+		e := object.TreeEntry{Name: name, Mode: filemode.Regular, Hash: h}
+		entries = append(entries, e)
+
+		for _, a := range survey.Attachments() {
+			refName := fmt.Sprintf("refs/attachments/%s/%s", a.Name, a.Checksum)
+			ref, err := b.r.Reference(plumbing.ReferenceName(refName), false)
+			if err != nil {
+				return "", err
+			}
+			_ = ref
+			e := object.TreeEntry{Name: a.Name, Mode: filemode.Regular, Hash: ref.Hash()}
+			attachmentEntries = append(attachmentEntries, e)
+		}
+	}
+	surveysTree, err := b.addTree(entries)
+	if err != nil {
+		return "", err
+	}
+	attachmentsTree, err := b.addTree(attachmentEntries)
+	if err != nil {
+		return "", err
+	}
+
+	rootEntries := []object.TreeEntry{
+		{Name: "attachments", Mode: filemode.Dir, Hash: attachmentsTree},
+		{Name: "surveys", Mode: filemode.Dir, Hash: surveysTree},
+	}
+	rootTree, err := b.addTree(rootEntries)
+	if err != nil {
+		return "", err
+	}
+
+	var parents []plumbing.Hash
+
+	// Check if our root tree is different than head
+	head, err := b.r.Head()
+	if err == nil {
+		c, err := b.r.CommitObject(head.Hash())
+		if err != nil {
+			return "", err
+		}
+		parents = append(parents, c.Hash)
+		if c.TreeHash == rootTree {
+			return c.Hash.String(), nil
+		}
+	}
+	commit, err := b.addCommit(uid, username, message, rootTree, parents)
+	if err != nil {
+		return "", err
+	}
+	err = b.updateHEAD(commit)
+	if err != nil {
+		return "", err
+	}
+	return commit.String(), nil
+}
+
+func (b *Backend) addBlob(data []byte) (plumbing.Hash, error) {
+	obj := b.r.Storer.NewEncodedObject()
+	obj.SetType(plumbing.BlobObject)
+	obj.SetSize(int64(len(data)))
+
+	w, err := obj.Writer()
+	if err != nil {
+		return plumbing.ZeroHash, err
+	}
+	defer w.Close()
+
+	n, err := w.Write(data)
+	if err != nil {
+		return plumbing.ZeroHash, err
+	}
+	if n != len(data) {
+		return plumbing.ZeroHash, fmt.Errorf("Error writing blob data")
+	}
+
+	return b.r.Storer.SetEncodedObject(obj)
+}
+
+func (b *Backend) addCommit(uid, username, message string, tree plumbing.Hash, parents []plumbing.Hash) (plumbing.Hash, error) {
+	author := object.Signature{
+		Name:  username,
+		Email: uid,
+		When:  time.Now(),
+	}
+	commit := object.Commit{
+		Author:       author,
+		Committer:    author,
+		Message:      message,
+		TreeHash:     tree,
+		ParentHashes: parents,
+	}
+	obj := b.r.Storer.NewEncodedObject()
+	err := commit.Encode(obj)
+	if err != nil {
+		return plumbing.ZeroHash, err
+	}
+	return b.r.Storer.SetEncodedObject(obj)
+}
+
+func (b *Backend) addTree(entries []object.TreeEntry) (plumbing.Hash, error) {
+	sort.Slice(entries, func(i, j int) bool {
+		return entries[i].Name < entries[j].Name
+	})
+	tree := object.Tree{Entries: entries}
+	obj := b.r.Storer.NewEncodedObject()
+	err := tree.Encode(obj)
+	if err != nil {
+		return plumbing.ZeroHash, err
+	}
+	return b.r.Storer.SetEncodedObject(obj)
+}
+
+func (b *Backend) readBlob(h plumbing.Hash) ([]byte, error) {
+	blob, err := b.r.BlobObject(h)
+	if err != nil {
+		return nil, err
+	}
+	r, err := blob.Reader()
+	if err != nil {
+		return nil, err
+	}
+	defer r.Close()
+	return io.ReadAll(r)
+}
+
+func (b *Backend) updateHEAD(commit plumbing.Hash) error {
+	name := plumbing.HEAD
+	head, err := b.r.Storer.Reference(name)
+	if err != nil {
+		return err
+	}
+
+	if head.Type() != plumbing.HashReference {
+		name = head.Target()
+	}
+
+	ref := plumbing.NewHashReference(name, commit)
+	return b.r.Storer.SetReference(ref)
+}
+
+func (b *Backend) Head() string {
+	ref, err := b.r.Head()
+	if err != nil {
+		return ""
+	}
+	return ref.Hash().String()
+}
+
+type Survey map[string]string
+
+type Attachment struct {
+	Name     string
+	Checksum string
+}
+
+type SurveyMap map[string]Survey
+
+type SurveyVersion struct {
+	Version string    `json:"version"`
+	Date    time.Time `json:"date"`
+	Survey  Survey    `json:"survey"`
+}
+
+func (s Survey) ID() string {
+	id := s["IdentifierUUID"]
+	if id == "" {
+		log.Printf("Warning: Missing survey id: %+v", s)
+	}
+	return id
+}
+
+func (s Survey) Attachments() []Attachment {
+	var attachments []Attachment
+	for _, a := range strings.Split(s["RelationAttachments"], "\n\n") {
+		var name, ts string
+		for _, s := range strings.Split(a, "\n") {
+			key, val := Cut(s, "=")
+			if key == "n" {
+				name = val
+			} else if key == "d" {
+				ts = val
+			}
+		}
+		if name != "" && ts != "" {
+			attachment := Attachment{Name: name, Checksum: ts}
+			attachments = append(attachments, attachment)
+		}
+	}
+	return attachments
+}
+
+func (s Survey) IsEqual(t Survey) bool {
+	for key := range s.Keys().Union(t.Keys()) {
+		if s[key] != t[key] {
+			return false
+		}
+	}
+	return true
+}
+
+func (s Survey) Keys() Set {
+	keys := make(Set, len(s))
+	for key := range s {
+		keys[key] = struct{}{}
+	}
+	return keys
+}
+
+func NewSurveyMap(surveys []Survey) SurveyMap {
+	m := make(map[string]Survey)
+	for _, s := range surveys {
+		id := s.ID()
+		m[id] = s
+	}
+	return m
+}
+
+func (m SurveyMap) IDs() Set {
+	ids := make(Set, len(m))
+	for id := range m {
+		ids[id] = struct{}{}
+	}
+	return ids
+}
--- main.go
+++ main.go
@@ -1,22 +1,307 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io"
 	"log"
+	"mime"
 	"net/http"
+	"path/filepath"
+
+	"github.com/gorilla/mux"
 )
+
+type SyncRequest struct {
+	UID      string   `json:"uid"`      // User or device identifier
+	UserName string   `json:"username"` // User name
+	Message  string   `json:"message"`  // Commit message (can be empty)
+	Head     string   `json:"head"`     // Client's last sync version (can be empty)
+	Surveys  []Survey `json:"surveys"`  // Surveys to be committed
+}
+
+type SyncResponse struct {
+	Status  string   `json:"status"`            // One of: ok, pushed, missing, conflict
+	Version string   `json:"version"`           // Current version of the server
+	Missing []string `json:"missing,omitempty"` // List of missing attachments
+	Updates []Patch  `json:"updates,omitempty"` // List of patches need to be applied on the client
+}
+
+// Sync Status
+const (
+	StatusOK       = "ok"       // Client is already in sync
+	StatusPushed   = "pushed"   // New version committed
+	StatusConflict = "conflict" // Client is in an older version
+	StatusMissing  = "missing"  // Some attachments are missing and need to be uploaded first
+)
+
+type Patch struct {
+	Id  string `json:"id"`
+	Old Survey `json:"old"`
+	New Survey `json:"new"`
+}
+
+func SyncTrench(w http.ResponseWriter, r *http.Request, b *Backend) error {
+	var req SyncRequest
+	dec := json.NewDecoder(r.Body)
+	err := dec.Decode(&req)
+	if err != nil {
+		return err
+	}
+
+	log.Printf("> SYNC %s %s", b.Trench, req)
+
+	head := b.Head()
+
+	// When the client head is empty we always return `conflict`, so that they
+	// get the latest version.
+	// When our head is empty, we let the client push. This could happen if they
+	// had synced before and we somehow reset our state.
+	if req.Head == "" || (req.Head != head && head != "") {
+		// Ignore errors here, we just fallback to empty list
+		old, _ := b.ReadSurveysAtVersion(req.Head)
+		new, err := b.ReadSurveys()
+		if err != nil {
+			return err
+		}
+
+		// Generate patches
+		var patches []Patch
+		oldMap := NewSurveyMap(old)
+		newMap := NewSurveyMap(new)
+		for id := range oldMap.IDs().Union(newMap.IDs()) {
+			oldSurvey := oldMap[id]
+			newSurvey := newMap[id]
+			if !oldSurvey.IsEqual(newSurvey) {
+				patch := Patch{Id: id, Old: oldSurvey, New: newSurvey}
+				patches = append(patches, patch)
+			}
+		}
+
+		resp := SyncResponse{
+			Status:  StatusConflict,
+			Version: head,
+			Updates: patches,
+		}
+		log.Printf("< SYNC %s %s", b.Trench, resp)
+		return writeJSON(w, r, &resp)
+	}
+
+	// The client is in the right version, but we need to check that we
+	// have all required attachments first.
+
+	missingAttachments := make(Set)
+	for _, survey := range req.Surveys {
+		for _, a := range survey.Attachments() {
+			if !b.ExistsAttachment(a.Name, a.Checksum) {
+				missingAttachments.Insert(a.Name)
+			}
+		}
+	}
+	if len(missingAttachments) > 0 {
+		resp := SyncResponse{
+			Status:  StatusMissing,
+			Version: head,
+			Missing: missingAttachments.Array(),
+		}
+		log.Printf("< SYNC %s %s", b.Trench, resp)
+		return writeJSON(w, r, &resp)
+	}
+
+	newHead, err := b.WriteTrench(req.UID, req.UserName, req.Message, req.Surveys)
+	if err != nil {
+		return err
+	}
+
+	resp := SyncResponse{Version: newHead}
+	if newHead != head {
+		resp.Status = StatusPushed
+	} else {
+		resp.Status = StatusOK
+	}
+	log.Printf("< SYNC %s %s", b.Trench, resp)
+	return writeJSON(w, r, &resp)
+}
+
+func ReadAttachment(w http.ResponseWriter, r *http.Request, b *Backend) error {
+	vars := mux.Vars(r)
+	name := vars["name"]
+	if name == "" {
+		return fmt.Errorf("Missing attachment name")
+	}
+	checksum := r.URL.Query().Get("checksum")
+	if checksum == "" {
+		return fmt.Errorf("Missing attachment checksum")
+	}
+
+	data, err := b.ReadAttachment(name, checksum)
+	if err != nil {
+		return err
+	}
+
+	ctype := mime.TypeByExtension(filepath.Ext(name))
+	if ctype != "" {
+		w.Header().Set("Content-Type", ctype)
+	}
+	w.Header().Set("Content-Length", fmt.Sprint(len(data)))
+	w.WriteHeader(http.StatusOK)
+
+	n, err := w.Write(data)
+	// We can't really return any errors at this point, just report it
+	if err != nil {
+		log.Printf("Error sending attachment %s: %s", name, err)
+	} else if n != len(data) {
+		log.Printf("Incomplete write for attachment %s (%d/%d)", name, n, len(data))
+	}
+	return nil
+}
+
+func WriteAttachment(w http.ResponseWriter, r *http.Request, b *Backend) error {
+	defer func() {
+		// Drain any leftovers and close
+		io.Copy(io.Discard, r.Body)
+		r.Body.Close()
+	}()
+
+	vars := mux.Vars(r)
+	name := vars["name"]
+	if name == "" {
+		return fmt.Errorf("Missing attachment name")
+	}
+	checksum := r.URL.Query().Get("checksum")
+	if checksum == "" {
+		return fmt.Errorf("Missing attachment checksum")
+	}
+
+	data, err := io.ReadAll(r.Body)
+	if err != nil {
+		return err
+	}
+	return b.WriteAttachment(name, checksum, data)
+}
+
+type ReadSurveysResponse struct {
+	Version string   `json:"version"`
+	Surveys []Survey `json:"surveys"`
+}
+
+func ReadSurveys(w http.ResponseWriter, r *http.Request, b *Backend) error {
+	version := r.URL.Query().Get("version")
+	if version == "" {
+		version = b.Head()
+	}
+	surveys, err := b.ReadSurveysAtVersion(version)
+	if err != nil {
+		return err
+	}
+
+	resp := ReadSurveysResponse{
+		Version: version,
+		Surveys: surveys,
+	}
+	return writeJSON(w, r, resp)
+}
+
+func ReadSurveyVersions(w http.ResponseWriter, r *http.Request, b *Backend) error {
+	vars := mux.Vars(r)
+	id := vars["uuid"]
+	versions, err := b.ReadAllSurveyVersions(id)
+	if err != nil {
+		return err
+	}
+	return writeJSON(w, r, versions)
+}
+
+func ListVersions(w http.ResponseWriter, r *http.Request, b *Backend) error {
+	versions, err := b.ListVersions()
+	if err != nil {
+		return err
+	}
+	return writeJSON(w, r, versions)
+}
+
+func writeJSON(w http.ResponseWriter, r *http.Request, v interface{}) error {
+	w.Header().Set("Content-Type", "application/json")
+
+	enc := json.NewEncoder(w)
+	if r.URL.Query().Has("debug") {
+		enc.SetIndent("", "  ")
+	}
+
+	w.WriteHeader(http.StatusOK)
+	// We can't really report any errors after this point
+	enc.Encode(v)
+	return nil
+}
+
+func (r SyncRequest) String() string {
+	return fmt.Sprintf("{head: %s, uid: %s, username: %s, surveys: [%d surveys]}",
+		Prefix(r.Head, 7), Prefix(r.UID, 8), r.UserName, len(r.Surveys))
+}
+
+func (r SyncResponse) String() string {
+	s := fmt.Sprintf("{status: %s, version: %s", r.Status, Prefix(r.Version, 7))
+	if len(r.Missing) > 0 {
+		s += fmt.Sprintf(", missing: [%d attachments]", len(r.Missing))
+	}
+	if len(r.Updates) > 0 {
+		s += fmt.Sprintf(", updates: [%d patches]", len(r.Updates))
+	}
+	return s + "}"
+}
+
+type ServerHandler func(http.ResponseWriter, *http.Request, *Backend) error
+
+func addRoute(r *mux.Router, method, path, rootDir string, handler ServerHandler) {
+	// Wrapper function to turn handler into http.HandleFunc compatible form
+	h := func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s", r.Method, r.URL)
+
+		vars := mux.Vars(r)
+		trench := vars["trench"]
+		if trench == "" {
+			http.Error(w, "Missing trench", http.StatusNotFound)
+			return
+		}
+
+		b, err := NewBackend(rootDir, trench)
+		if err != nil {
+			msg := fmt.Sprintf("Error initializing backend for %s: %s", trench, err)
+			log.Println(msg)
+			http.Error(w, msg, http.StatusBadRequest)
+			return
+		}
+
+		err = handler(w, r, b)
+		if err != nil {
+			log.Printf("ERROR %s", err)
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+	}
+	r.HandleFunc(path, h).Methods(method)
+}
 
 func main() {
 	rootFlag := flag.String("r", ".", "Root dir of Git repositories")
 	portFlag := flag.Int("p", 9000, "Listen on this port")
 	flag.Parse()
 
+	rootDir := *rootFlag
+	r := mux.NewRouter()
+
+	addRoute(r, "POST", "/{trench}", rootDir, SyncTrench)
+	addRoute(r, "GET", "/{trench}/attachments/{name}", rootDir, ReadAttachment)
+	addRoute(r, "PUT", "/{trench}/attachments/{name}", rootDir, WriteAttachment)
+	addRoute(r, "GET", "/{trench}/surveys", rootDir, ReadSurveys)
+	addRoute(r, "GET", "/{trench}/surveys/{uuid}/versions", rootDir, ReadSurveyVersions)
+	addRoute(r, "GET", "/{trench}/versions", rootDir, ListVersions)
+
 	addr := fmt.Sprintf(":%d", *portFlag)
-	srv := &Server{Root: *rootFlag}
 	s := &http.Server{
-		Addr:    addr,
-		Handler: srv,
+		Addr:    fmt.Sprintf(":%d", *portFlag),
+		Handler: r,
 	}
 
 	log.Printf("Listening on %s", addr)
