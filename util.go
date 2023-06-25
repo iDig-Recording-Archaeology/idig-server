a60957deb02706d930e8de29ec7fbcb94f91c07e
Georgios Verigakis <verigak@gmail.com>
2023-06-25 11:46:45 +0300

    Use DefaultCost for bcrypt

--- util.go
+++ util.go
@@ -83,6 +83,6 @@ func (s Set) Union(a Set) Set {
 }
 
 func HashPassword(password string) (string, error) {
-	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	return string(bytes), err
 }
